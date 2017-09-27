7c330d112d6d49513e3e02c4ef759f07b43f9460
Christian Zellweger <chzellweger@gmail.com>
2017-09-27 19:07:53 +0200

    refactor persistence-solution

--- src/add/add.js
+++ src/add/add.js
@@ -1,65 +1,30 @@
-
 ;(function() {
   'use strict'
+  const items = localStorage.notes
+    ? JSON.parse(localStorage.getItem('notes'))
+    : []
 
-  function getQueryVariable(variable) {
-    const query = window.location.search.substring(1)
-    const vars = query.split('&')
+  const note = getQueryStringAsObject()
 
-    for (let i = 0; i < vars.length; i++) {
-      let pair = vars[i].split('=')
+  if (note) {
+    note.id = new Date().getTime()
+    note.importance = note.importance || 1
 
-      if (pair[0] === variable) {
-        return pair[1]
-      }
-    }
-    return false
+    items.push(note)
+    localStorage.setItem('notes', JSON.stringify(items))
   }
 
-  try {
-    var dateValue =
-      decodeURIComponent(getQueryVariable('finishby').replace(/\+/g, '%20')) ||
-      ''
-    var titleValue =
-      decodeURIComponent(getQueryVariable('title').replace(/\+/g, '%20')) || ''
-    var importanceValue =
-      decodeURIComponent(
-        getQueryVariable('importance').replace(/\+/g, '%20')
-      ) || ''
-    var contentValue =
-      decodeURIComponent(getQueryVariable('content').replace(/\+/g, '%20')) ||
-      ''
+  function getQueryStringAsObject() {
+    let queryString = window.location.search.substring(1)
+    if (queryString.length === 0) return null
 
-    const items = localStorage.notes
-      ? JSON.parse(localStorage.getItem('notes'))
-      : []
+    let queryElements = queryString.split('&')
 
-    const note = {
-      date: dateValue,
-      title: titleValue,
-      importance: importanceValue,
-      note: contentValue,
-      id: items.length + Math.round(Math.random() * 100)
-    }
-    
-    items.push(note)
+    return queryElements.reduce((object, queryPair) => {
+      const keyValue = queryPair.split('=')
 
-    localStorage.setItem('notes', JSON.stringify(items))
-
-    let title = document.getElementById('title')
-    title.value = titleValue
-
-    let content = document.getElementById('content')
-    content.value = contentValue
-
-    let importance = Array.from(document.getElementsByName('importance'))
-      .find(el => el.value === importanceValue.toString())
-    importance.checked = true
-
-    let date = document.getElementById('date')
-    date.value = dateValue
-  } catch (error) {
-    console.log(error)
+      object[keyValue[0]] = decodeURIComponent(keyValue[1]).replace(/\+/g, ' ')
+      return object
+    }, {})
   }
 })()
-
--- src/index/index.js
+++ src/index/index.js
@@ -1,15 +1,11 @@
 'use strict'
 ;(function() {
-  try {
-    const items = localStorage.notes
-      ? JSON.parse(localStorage.getItem('notes'))
-      : []
-    
-    const postFix = items.length === 1 ? 'Notiz' : 'Notizen'
-    
-    const count = document.getElementById('count')
-    count.innerText = `${items.length} ${postFix}`
-  } catch (error) {
-    console.log(error)
-  }
+  const items = localStorage.notes
+    ? JSON.parse(localStorage.getItem('notes'))
+    : []
+
+  const postFix = items.length === 1 ? 'Notiz' : 'Notizen'
+
+  const count = document.getElementById('count')
+  count.innerText = `${items.length} ${postFix}`
 })()
