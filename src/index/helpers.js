21bb62ab72564b871c630ed05e1d0851c1c3ebbb
Christian Zellweger <chzellweger@gmail.com>
2017-09-27 19:56:42 +0200

    listen to DOMContentLoaded instead of load

--- src/index/helpers.js
+++ src/index/helpers.js
@@ -1,7 +1,7 @@
 ;(function() {
   const styleChanger = document.getElementById('select-style')
 
-  window.addEventListener('load', function() {
+  window.addEventListener('DOMContentLoaded', function() {
     const style = JSON.parse(localStorage.getItem('style'))
     document.body.className = style
     if (styleChanger) styleChanger.value = style
