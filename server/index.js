1e3c251184c0c8184271c386f98a9ba61331dd77
Christian Zellweger <chzellweger@gmail.com>
2017-10-28 18:43:19 +0200

    change server-index

--- server/index.js
+++ server/index.js
@@ -25,4 +25,4 @@ app.use("/", require('./routes/routes.js'));
 
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3010;
-app.listen(port, hostname, () => {  console.log(`Server running at http://${hostname}:${port}/`); });
+app.listen(port, () => {  console.log(`Server running at http://${hostname}:${port}/`); });
